40290b13fd4a9739910f3dc072f46a2c74ab3f49
starr <starr@SERGIOPC>
2023-05-16 18:16:20 -0500

    Agregar archivos de proyecto.

---
+++ Presentacion/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Presentacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.cbCamaras = new System.Windows.Forms.ComboBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.pbImage1 = new System.Windows.Forms.PictureBox();
+            this.pbImage2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(29, 31);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 1;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // cbCamaras
+            // 
+            this.cbCamaras.FormattingEnabled = true;
+            this.cbCamaras.Location = new System.Drawing.Point(127, 33);
+            this.cbCamaras.Name = "cbCamaras";
+            this.cbCamaras.Size = new System.Drawing.Size(179, 21);
+            this.cbCamaras.TabIndex = 2;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(143, 386);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = "Capturar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(369, 386);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // pbImage1
+            // 
+            this.pbImage1.Location = new System.Drawing.Point(49, 106);
+            this.pbImage1.Name = "pbImage1";
+            this.pbImage1.Size = new System.Drawing.Size(395, 247);
+            this.pbImage1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImage1.TabIndex = 7;
+            this.pbImage1.TabStop = false;
+            // 
+            // pbImage2
+            // 
+            this.pbImage2.Location = new System.Drawing.Point(493, 106);
+            this.pbImage2.Name = "pbImage2";
+            this.pbImage2.Size = new System.Drawing.Size(330, 247);
+            this.pbImage2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImage2.TabIndex = 8;
+            this.pbImage2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 458);
+            this.Controls.Add(this.pbImage2);
+            this.Controls.Add(this.pbImage1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.cbCamaras);
+            this.Controls.Add(this.btnIniciar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.ComboBox cbCamaras;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.PictureBox pbImage1;
+        private System.Windows.Forms.PictureBox pbImage2;
+    }
+}
+
---
+++ Presentacion/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Controls;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace Presentacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private FilterInfoCollection Dispositivos;
+        private VideoCaptureDevice FuenteDeVideo;
+        private string path = @"C:\Users\starr\OneDrive\Documentos\Yo\Universidad\4to Semestre\Programación III\Camara\Presentacion\Imagenes\";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Dispositivos = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Dispositivo in Dispositivos)
+            {
+                cbCamaras.Items.Add(Dispositivo.Name);
+            }
+            cbCamaras.SelectedIndex = 0;
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            ApagarCamara();
+            int i = cbCamaras.SelectedIndex;
+            string NombreVideo = Dispositivos[i].MonikerString;
+            FuenteDeVideo = new VideoCaptureDevice(NombreVideo);
+            FuenteDeVideo.NewFrame += new NewFrameEventHandler(Capturando);
+            FuenteDeVideo.Start();
+        }
+
+        private void ApagarCamara()
+        {
+            if(FuenteDeVideo != null && FuenteDeVideo.IsRunning)
+            {
+                FuenteDeVideo.SignalToStop();
+                FuenteDeVideo = null;
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            ApagarCamara();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (FuenteDeVideo != null && FuenteDeVideo.IsRunning)
+            {
+                pbImage2.Image = pbImage1.Image;
+                pbImage2.Image.Save(path+"pueba.jpg", ImageFormat.Jpeg);
+            }
+        }
+
+        private void Capturando(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap Imagen = (Bitmap)eventArgs.Frame.Clone();
+            pbImage1.Image = Imagen;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ApagarCamara();
+        }
+    }
+}
